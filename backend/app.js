936fc9a4596a87ef27f283130a9bab304a8e7af5
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 12:10:14 -0500

    error handling

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,8 @@ const cookieParser = require("cookie-parser");
 const { environment } = require('./config');
 const isProduction = environment === 'production';
 
+const { ValidationError } = require('sequelize');
+
 const routes = require('./routes');
 
 const app = express();
@@ -35,5 +37,32 @@ app.use(
 );
 
 app.use(routes);
+
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map((e) => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+});
+
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
