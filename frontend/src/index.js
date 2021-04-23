e79fd7c62337064ddb2fdc77abcd27c032afd8ee
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 17:26:07 -0500

    frontend start redone

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,31 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider as ReduxProvider } from 'react-redux';
+import './index.css';
 import App from './App';
-
 import configureStore from './store';
 
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
-};
+}
 
 function Root() {
   return (
-    <Provider store={store}>
+    <ReduxProvider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Provider>
-  )
+    </ReduxProvider>
+  );
 }
 
 ReactDOM.render(
   <React.StrictMode>
     <Root />
   </React.StrictMode>,
-  document.getElementById('root'),
+  document.getElementById('root')
 );
