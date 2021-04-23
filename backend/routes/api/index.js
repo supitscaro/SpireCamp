bba3026a52b431a18abf5190c0ddc19ec03a3ce2
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 12:02:46 -0500

    api testing

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,16 @@
 const router = require('express').Router();
 
-const apiRouter = require('./api');
-
-router.use('/api', apiRouter);
+router.post('/test', function (req, res) {
+    res.json({ requestBody: req.body });
+});
 
 module.exports = router;
+
+// fetch('/api/test', {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "SrjBlGSq-se1JXq7JDDXkd90GRbkA4K673Lo"
+//     },
+//     body: JSON.stringify({ hello: 'world' })
+//   }).then(res => res.json()).then(data => console.log(data));
