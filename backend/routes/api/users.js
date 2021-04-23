0147800a7cc0475ea4c96f89ea14b608287e03fd
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 16:06:06 -0500

    sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,5 +1,38 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
+
+// Sign up
+router.post(
+    '',
+    asyncHandler(async (req, res) => {
+        const { email, password, username } = req.body;
+        const user = await User.signup({ email, username, password });
+
+        await setTokenCookie(res, user);
+
+        return res.json({
+            user,
+        });
+    }),
+);
 
 
 module.exports = router;
+
+// fetch('/api/users', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": "3AybqIhL-BAq3ZTC6BMxXAGudmR5Q65NMf3o"
+//     },
+//     body: JSON.stringify({
+//       email: 'spidey@spider.man',
+//       username: 'Spidey',
+//       password: 'password'
+//     })
+//   }).then(res => res.json()).then(data => console.log(data));
