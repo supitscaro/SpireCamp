f6df32da9c0fce3e75805a8637087c35970386e5
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 16:08:53 -0500

    restore user

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -39,6 +39,21 @@ router.delete(
     }
 );
 
+// Restore User
+
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+        const { user } = req;
+        if (user) {
+            return res.json({
+                user: user.toSafeObject()
+            });
+        } else return res.json({});
+    }
+);
+
 module.exports = router;
 
 // LOG IN
