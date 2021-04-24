a4d698c4bf01d3e5959a3aed0ba8be2d5552f774
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-23 20:41:17 -0500

    restore session user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -30,6 +30,13 @@ export const login = (user) => async (dispatch) => {
     return res;
 };
 
+export const restoreUser = () => async (dispatch) => {
+    const res = await csrfFetch('/api/session');
+    const data = await res.json();
+    dispatch(setUser(data.user));
+    return res;
+};
+
 const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
