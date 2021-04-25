cb5e14477f82ea71c68fb2b86cd11021aac0e662
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-24 20:08:09 -0500

    reviews seeder

---
+++ backend/db/seeders/20210425010406-review.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkInsert('People', [{
+        name: 'John Doe',
+        isBetaMember: false
+      }], {});
+    */
+    return queryInterface.bulkInsert('Reviews', [
+      {
+        title: 'Perfect Getaway',
+        review: 'Amazing place to get away for a few days. Beautiful scenery and relaxing activities to do nearby.',
+        recommended: true,
+        hostings_id: 2,
+        user_id: 2
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.bulkDelete('People', null, {});
+    */
+  }
+};
