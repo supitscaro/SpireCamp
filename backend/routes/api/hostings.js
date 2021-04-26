2dd54668d3cc6d35e6e106929f1985caa065638b
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-26 12:13:14 -0500

    hosting validators created

--- backend/routes/api/hostings.js
+++ backend/routes/api/hostings.js
@@ -2,11 +2,13 @@ const express = require('express');
 const asyncHandler = require('express-async-handler');
 
 const { Hosting, User, State, Accommodations, Activities, Review } = require('../../db/models');
+const { check } = require('express-validator');
 
 const router = express.Router();
 
+// get hosting posts - should be every post available?
 router.get(
-    '/hosting',
+    '/hosting', // unsure of route if I want every post
     asyncHandler(async (req, res, next) => {
         let hostId = parseInt(req.params.id, 10);
         let hosting = await Hosting.findByPk(hostId,
@@ -22,4 +24,25 @@ router.get(
         });
         res.render('post', { hosting, reviews });
     })
-)
+);
+
+let hostingValidator = [
+    check('name')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage("Your hosting must have a name provided."),
+    check('description')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage("You must provide some description about the location."),
+    check('locationDetails')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage("Please provide the name of the city where this hosting is located."),
+    check("state")
+        .exists({ checkFalsy: true })
+        .withMessage("Please select a state."),
+    check("cost")
+        .exists({ checkFalsy: true })
+        .withMessage("Please provide the cost of stay.")
+];
