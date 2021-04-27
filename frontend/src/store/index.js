355a64830b3a0a6e6904aa47d7b06eb1e53d509f
Caro <55109108+CaroMen@users.noreply.github.com>
2021-04-27 10:24:29 -0500

    start of store on frontend

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import sessionReducer from './session';
+import hostingReducer from './hostings';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  hosting: hostingReducer,
 });
 
 let enhancer;
