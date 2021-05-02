b1e041a3560837b117a0627cca1844efb369ad09
Caro <55109108+CaroMen@users.noreply.github.com>
2021-05-02 00:19:21 -0500

    footer started

---
+++ frontend/src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <h2>Made with 💜 by Caro</h2>
+            <div>
+                <i class="fab fa-linkedin"></i>
+                {/* href="https://www.linkedin.com/in/caroline-mendez-41a181134/" */}
+            </div>
+        </div>
+    )
+}
